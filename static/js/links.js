afcb4410867c8c958dfe4fd5567495afb5eaddb8
mashedpotats <samuelli4521@gmail.com>
2016-11-17 18:31:54 -0800

    successfully created a shared links page.

--- static/js/links.js
+++ static/js/links.js
@@ -4,10 +4,10 @@
 $(function () {
     // variable declarations
     var obj = this; // give reference
-    var $delete = $('#delete');
     var $filesTable = $('#files-table');
     var $files = $('#files');
-    this.$selected = $();
+    var $information = $('#information');
+    this.$selected = $(); // jquery equivalent null
 
     String.prototype.format = function (values) {
         var string = this;
@@ -20,32 +20,22 @@ $(function () {
         return string;
     };
 
-    // http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
-    this.formatBytes = function (bytes, decimals) {
-        if (bytes == 0) return '0 Byte';
-        var k = 1024;
-        var dm = decimals + 1 || 3;
-        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        var i = Math.floor(Math.log(bytes) / Math.log(k));
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-    };
-
-    window.fileElement = '<tr class="file"><th class="icon" scope="row">{icon}</th><td class="file-name">{name}</td><td>{type}</td></tr>';
-    var extensionRegex = /(?:\.([^.]+))?$/;
+    window.fileElement = '<tr class="file">' +
+        '<th class="path">{path}</th>' +
+        '<th class="url"><a href="{url}" target="_blank">{url}</a></th>' +
+        '<th>{linkDate}</th>' +
+        '<th>{expiration}</th>' +
+        '</tr>';
     this.updateContents = function () {
         $.ajax({
             dataType: 'json',
-            url: '/items',
+            url: '/links',
             success: function (files) {
                 console.log('Retrieved', files);
                 $files.html(''); // clean
 
                 files.forEach(function (file) {
-                    var $fileElement = $(fileElement.format({
-                        name: file.name.replace(/\.[^/.]+$/, ""),
-                        type: obj.getExtension(file),
-                        icon: obj.decideIcon(file.type)
-                    })).data('filename', file.name);
+                    var $fileElement = $(fileElement.format(obj.processFile(file))).data('file', obj.processFile(file));
 
                     $files.append($fileElement);
                 });
@@ -57,50 +47,55 @@ $(function () {
     };
 
     this.listeners = function () {
-        $delete.on('click', function () {
-            if (!$delete.hasClass('disabled')) {
-                $.ajax({
-                    method: 'DELETE',
-                    url: '/crud/delete',
-                    headers: {
-                        'filename': obj.$selected.data('filename')
-                    },
-                    success: function () {
-                        obj.updateContents();
-                    }
-                });
-            }
-        });
+
     };
 
-
+    var emptyInfo = {
+        'path': '',
+        'url': '',
+        'linkDate': '',
+        'expiration': ''
+    };
     this.fileListeners = function () {
         $('.file', $files).on('click', function () {
             obj.$selected.removeClass('active');
-            $delete.removeClass('disabled');
-            if (obj.$selected[0] != this)
+            obj.updateInfo(emptyInfo);
+            if (obj.$selected[0] != this) {
                 $(this).toggleClass('active');
-            else // if deselecting
-                $delete.addClass('disabled');
+                obj.updateInfo($(this).data('file'));
+            }
 
             obj.$selected = $(this);
         });
     };
 
-    this.getExtension = function (file) {
-        var extension = extensionRegex.exec(file.name)[1];
-        console.log(file.type);
-        if (file.type == 'folder')
-            return 'folder';
-        else if (extension != undefined)
-            return extension;
-        return 'none';
+    // requires path, url, link date, expiration
+    this.processFile = function (file) {
+        var data = {};
+
+        data.path = file._path_lower_value;
+
+        data.url = 'No Url';
+        if (file._url_present)
+            data.url = file._url_value.substring(0, file._url_value.length - 5);
+
+        data.linkDate = 'None';
+
+        data.expiration = 'None';
+        if (file._expires_value != null)
+            file.expiration = file._expires_value;
+
+        return data;
     };
 
-    this.decideIcon = function(type) {
-        if (type == 'folder')
-            return 'folder';
-        return 'insert_drive_file';
+    this.updateInfo = function (info) {
+        console.log('updated with', info);
+
+        $('.path', $information).text(info.path);
+        $('.url', $information).attr('href', info.url);
+        $('.url', $information).text(info.url);
+        $('.linkDate', $information).text(info.linkDate);
+        $('.expiration', $information).text(info.expiration);
     };
 
     // run initial methods
