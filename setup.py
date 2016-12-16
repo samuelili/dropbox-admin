aaeb65649a96eae678244ae7ab67405965047a7a
Jamie Li <lijamie@gmail.com>
2016-12-16 11:00:48 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='dropbox-admin',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'static'},
+    url='',
+    license='',
+    author='Jamie Li',
+    author_email='',
+    description=''
+)
